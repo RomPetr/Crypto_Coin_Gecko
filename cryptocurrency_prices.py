6aaf5c8d34c52afade941e2abca51e575f794c1c
Rompetr78 <rompetr@yahoo.com>
2024-11-05 16:56:48 +0800

    Создан основной файл 'cryptocurrency_prices.py' с интерфейсом главного окна 'window'

---
+++ cryptocurrency_prices.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+import requests
+
+# Создаем интерфейс
+window = tk.Tk()
+window.title("Курс криптовалют")
+window.geometry("300x200")
+
+# Запуск интерфейса
+window.mainloop()
