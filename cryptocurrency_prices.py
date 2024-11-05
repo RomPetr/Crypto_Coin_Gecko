df8f32d241a187199b0ea459b206afe90240456b
Rompetr78 <rompetr@yahoo.com>
2024-11-05 17:48:30 +0800

    Создана функция 'get_crypto_list' для получения списка криптовалют

--- cryptocurrency_prices.py
+++ cryptocurrency_prices.py
@@ -2,10 +2,23 @@ import tkinter as tk
 from tkinter import ttk
 import requests
 
+
+# Функция для получения списка криптовалют
+def get_crypto_list():
+    response = requests.get("https://api.coingecko.com/api/v3/coins/list")
+    response.raise_for_status()
+    get_coins = response.json()
+    return get_coins
+
+
 # Создаем интерфейс
 window = tk.Tk()
 window.title("Курс криптовалют")
 window.geometry("300x200")
+
+# Получаем список криптовалют
+coins = get_crypto_list()
+print(len(coins)) # Сколько всего криптовалют публикуется на CionGecko.com
 
 # Запуск интерфейса
 window.mainloop()
