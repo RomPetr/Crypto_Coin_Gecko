d66d4e94fce06922e95255fa65256fa8683a6b37
Rompetr78 <rompetr@yahoo.com>
2024-11-05 17:53:53 +0800

    Файл cryptocurrency_prices.py переименован в cryptocurrency_prices_v1.py, так же добавлена обработка исключений try-except в функцию 'get_crypto_list'

--- cryptocurrency_prices_v1.py
+++ cryptocurrency_prices_v1.py
@@ -1,14 +1,18 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox as mb
 import requests
 
 
 # Функция для получения списка криптовалют
 def get_crypto_list():
-    response = requests.get("https://api.coingecko.com/api/v3/coins/list")
-    response.raise_for_status()
-    get_coins = response.json()
-    return get_coins
+    try:
+        response = requests.get("https://api.coingecko.com/api/v3/coins/list")
+        response.raise_for_status()
+        get_coins = response.json()
+        return get_coins
+    except Exception as e:
+        mb.showerror("Ошибка", f"Код ошибки: {e}")
 
 
 # Создаем интерфейс
